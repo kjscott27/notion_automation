4f392683294e51f235a67757346ee0a0d76eda00
Kyle Scott <kyle.j.scott@hotmail.com>
2020-06-28 19:39:59 -0400

    automate navigation to notion login page

--- notion_automation.py
+++ notion_automation.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver.get("https://notion.so")
+assert "Notion" in driver.title
+login_button = None
+try:
+    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="notion-app"]/div/div[1]/div[1]/div/nav/div[5]/a[1]')))
+except Exception as e:
+    logging.exception(e)
+    driver.close()
+
+if login_button is None:
+    driver.close()
+else:
+    login_button.click()
